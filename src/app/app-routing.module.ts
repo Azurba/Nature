468a0b23a87b7b6981e4b539a79d0ff7bf2ee9d2
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-08 11:22:23 -0600

    add home and birds page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { BirdsComponent } from './Pages/birds/birds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, //make the 'home' the default path
+  {path: 'birds', component: BirdsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
