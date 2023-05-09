91e3fdf6614dcef4c819e244ddea8ab2403ce301
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-09 14:43:01 -0600

    add flowers implementation

--- src/app/Pages/flowers/flowers/flowers.component.ts
+++ src/app/Pages/flowers/flowers/flowers.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./flowers.component.scss']
 })
 export class FlowersComponent {
+  photos : string[] = [];
+  selectedImage: string | null = null;
 
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 1; i < 65; i++) {
+      this.photos.push(`./assets/img/flowers/${i}.jpg`);
+    }
+  }
+
+  openOverlay(photo: string) {
+    this.selectedImage = photo;
+  }
+
+  closeOverlay() {
+    this.selectedImage = null;
+  }
 }
