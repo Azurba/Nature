eff705504bb5ed79c9ab5f12937630105be99f3e
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-09 14:31:50 -0600

    add animals implementation

--- src/app/Pages/animals/animals/animals.component.ts
+++ src/app/Pages/animals/animals/animals.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./animals.component.scss']
 })
 export class AnimalsComponent {
+  photos : string[] = [];
+  selectedImage: string | null = null;
 
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 1; i < 33; i++) {
+      this.photos.push(`./assets/img/animals/${i}.JPG`);
+    }
+  }
+
+  openOverlay(photo: string) {
+    this.selectedImage = photo;
+  }
+
+  closeOverlay() {
+    this.selectedImage = null;
+  }
 }
