944246223d94d25703a4f114d901e72a15a5be54
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-09 14:45:40 -0600

    add landscape implementation

--- src/app/Pages/landscapes/landscapes/landscapes.component.ts
+++ src/app/Pages/landscapes/landscapes/landscapes.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./landscapes.component.scss']
 })
 export class LandscapesComponent {
+  photos : string[] = [];
+  selectedImage: string | null = null;
 
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 1; i < 44; i++) {
+      this.photos.push(`./assets/img/landscape/${i}.jpg`);
+    }
+  }
+
+  openOverlay(photo: string) {
+    this.selectedImage = photo;
+  }
+
+  closeOverlay() {
+    this.selectedImage = null;
+  }
 }
